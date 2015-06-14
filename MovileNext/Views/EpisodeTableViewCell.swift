3ca3e5b0c265e1c49824746378b86a20af72d119
User <User@MAC-134.nabs.com.br>
2015-06-14 12:02:43 -0300

    Updates

---
+++ MovileNext/Views/EpisodeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EpisodeTableViewCell.swift
+//  MovileNext
+//
+//  Created by User on 14/06/15.
+//  Copyright (c) 2015 Movile. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
